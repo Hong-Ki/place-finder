22f0a77bfbd26a3a1f641790dd3702735c4e711a
Minong <minhk3259@gmail.com>
2019-07-15 00:55:31 +0900

    - keyword 이벤트 모드 추가

--- src/frontend/src/modules/modal.js
+++ src/frontend/src/modules/modal.js
@@ -2,17 +2,23 @@ import { Map } from 'immutable';
 import { createAction, handleActions } from 'redux-actions';
 
 const SET_VISIBLE = 'modal/SET_VISIBLE';
+export const SET_MODE = 'modal/SET_MODE';
 
 export const setVisible = createAction(SET_VISIBLE);
+export const setMode = createAction(SET_MODE);
 
 const initialState = Map({
   visible: true,
+  mode: '',
 });
 
 export default handleActions(
   {
     [SET_VISIBLE]: (state, action) => {
       return state.set('visible', action.payload);
+    },
+    [SET_MODE]: (state, action) => {
+      return state.set('mode', action.payload).set('visible', true);
     },
   },
   initialState,
