38894333ae4014abeed4794ddb2e7630a22a1b41
Minong <minhk3259@gmail.com>
2019-07-15 00:49:55 +0900

    saga 추가로 인한 미들웨어 추가

--- src/frontend/src/index.js
+++ src/frontend/src/index.js
@@ -4,14 +4,17 @@ import './styles/layout.module.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reducers from './modules';
+import { rootSaga } from './sagas/index';
 import { Provider } from 'react-redux';
+import createSagaMiddleware from 'redux-saga';
 
-const store = createStore(
-  reducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-);
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(reducers, applyMiddleware(sagaMiddleware));
+
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
   <Provider store={store}>
