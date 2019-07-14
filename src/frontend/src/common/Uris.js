b721484fc56c380e06342ad99e8b98e0489e7399
Minong <minhk3259@gmail.com>
2019-07-15 00:44:25 +0900

    uri추가 - 인기 키워드 - 검색 내역

--- src/frontend/src/common/Uris.js
+++ src/frontend/src/common/Uris.js
@@ -1,3 +1,5 @@
 export const searchUri = 'http://localhost:8080/search';
 export const loginUri = 'http://localhost:8080/auth/login';
 export const tokenCheckUri = 'http://localhost:8080/auth';
+export const popularKeywordUri = 'http://localhost:8080/keyword/popular';
+export const historyKeywordUri = 'http://localhost:8080/keyword/history';
