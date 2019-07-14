5eea8a9bd8fcb91cf16c85629885c3d52dbb8d1e
Minong <minhk3259@gmail.com>
2019-07-15 00:54:19 +0900

    keyword 리듀서 추가

--- src/frontend/src/modules/index.js
+++ src/frontend/src/modules/index.js
@@ -3,9 +3,11 @@ import { combineReducers } from 'redux';
 import places from './places';
 import maps from './maps';
 import modal from './modal';
+import keywords from './keywords';
 
 export default combineReducers({
   places,
   maps,
   modal,
+  keywords,
 });
